82d66eb971ca6f85854ac9976361d24e304af9e6
lyudmila-zoria <lyudmila.zoria@helpfulhuman.com>
2018-04-04 23:08:00 +0300

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SendingLetters from './components/SendingLetters'
 import Login from './components/Login'
 import Comments from './components/Comments'
 import Teacher from './components/Teacher'
+import NotFound from "./components/NotFound";
 
 class App extends Component {
 
@@ -31,12 +32,14 @@ class App extends Component {
         <div className='routes'>
           <Switch>
             <PrivateRoute exact path='/' component={Home} />
+            <PrivateRoute exact path='/admin' component={Home} />
             <PrivateRoute path='/admin/feedback' component={Feedback} />
             <PrivateRoute path='/admin/teachers' component={Teachers} />
             <PrivateRoute path='/admin/teacher/:id' component={Teacher} />
             <PrivateRoute path='/admin/sending_letters' component={SendingLetters} />
             <Route path='/admin/login' component={Login} />
             <Route path='/feedback/:id' component={Comments}/>
+            <Route component={NotFound}/>
           </Switch>
         </div>
       </div>
