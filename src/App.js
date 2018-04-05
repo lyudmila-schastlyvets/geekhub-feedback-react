b7ceb33e4a79303433723333e8604b6d15ef1f6f
marina <marina_bakum@mail.ru>
2018-04-06 01:34:32 +0300

    style logo, 404 page, footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Route, Switch } from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+import logo from './images/logo-geekhub.png'
 
 import PrivateRoute from './components/Private'
 
@@ -12,41 +13,43 @@ import SendingLetters from './components/SendingLetters'
 import Login from './components/Login'
 import Comments from './components/Comments'
 import Teacher from './components/Teacher'
-import NotFound from "./components/NotFound";
+import NotFound from './components/NotFound'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedIn: localStorage.getItem('loggedIn')
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: localStorage.getItem('loggedIn')
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="header-bg">
-            <h1 className="App-title">Geekhub charity project</h1>
-          </div>
-        </header>
-        <div className='routes'>
-          <Switch>
-            <PrivateRoute exact path='/' component={Home} />
-            <PrivateRoute exact path='/admin' component={Home} />
-            <PrivateRoute path='/admin/feedback' component={Feedback} />
-            <PrivateRoute path='/admin/teachers' component={Teachers} />
-            <PrivateRoute path='/admin/teacher/:id' component={Teacher} />
-            <PrivateRoute path='/admin/sending_letters' component={SendingLetters} />
-            <Route path='/admin/login' component={Login} />
-            <Route path='/feedback/:id' component={Comments}/>
-            <Route component={NotFound}/>
-          </Switch>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <div className='header-bg'>
+                        <img className='logo' src={logo}/>
+                        <h1 className='App-title'>Feedback System</h1>
+                    </div>
+                </header>
+                <div className='routes'>
+                    <Switch>
+                        <PrivateRoute exact path='/' component={Home}/>
+                        <PrivateRoute exact path='/admin' component={Home}/>
+                        <PrivateRoute path='/admin/feedback' component={Feedback}/>
+                        <PrivateRoute path='/admin/teachers' component={Teachers}/>
+                        <PrivateRoute path='/admin/teacher/:id' component={Teacher}/>
+                        <PrivateRoute path='/admin/sending_letters' component={SendingLetters}/>
+                        <Route path='/admin/login' component={Login}/>
+                        <Route path='/feedback/:id' component={Comments}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+                <div className='footer'>Copyright &#169; GeekHub 2018</div>
+            </div>
+        )
+    }
 }
 
 export default App
