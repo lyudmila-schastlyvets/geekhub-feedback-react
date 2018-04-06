a8446aebcbce2ea5983038b12c1b2c25e18325cc
lyudmila-zoria <lyudmila.zoria@helpfulhuman.com>
2018-04-06 21:56:18 +0300

    Fixed header

--- src/App.js
+++ src/App.js
@@ -29,8 +29,10 @@ class App extends Component {
             <div className='App'>
                 <header className='App-header'>
                     <div className='header-bg'>
-                        <img className='logo' src={logo} alt='geekhub-logo'/>
-                        <h1 className='App-title'>Feedback System</h1>
+                        <div className="container header-responsive">
+                          <img className='logo' src={logo} alt='geekhub-logo'/>
+                          <h1 className='App-title'>Feedback System</h1>
+                        </div>
                     </div>
                 </header>
                 <div className='routes'>
