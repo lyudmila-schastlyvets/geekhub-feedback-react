c1651911ed5fa79c391be97e8910f82e4beab377
lyudmila-zoria <lyudmila.zoria@helpfulhuman.com>
2018-04-20 12:41:44 +0300

    Not Found Teacher

---
+++ src/components/PageUpload.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class PageUpload extends Component {
+  render () {
+    return (
+      <div className='centered-content'>
+        <h2>Зачекайте!</h2>
+        <p>Cторінка завантажується!</p>
+      </div>
+    )
+  }
+}
